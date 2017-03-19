b682c514d3487e1d2d47df749fe25eef8cf0a852
Arun <imarunsinghal@gmail.com>
2017-03-19 21:26:58 +0530

    Changes for Bug#22

--- data/common/misc/copyright.php
+++ data/common/misc/copyright.php
@@ -1,7 +1,7 @@
 <div class="wrapper row5">
   <div id="copyright" class="hoc clear"> 
     <!-- ################################################################################################ -->
-    <p class="fl_left">Copyright &copy; 2016 - All Rights Reserved - <a href="http://www.hellocuriousbrain.com">Curious Brain</a></p>
+    <p class="fl_left">Copyright &copy; 2017 - All Rights Reserved - <a href="http://www.hellocuriousbrain.com">Curious Brain</a></p>
     <!-- ################################################################################################ -->
   </div>
 </div>
