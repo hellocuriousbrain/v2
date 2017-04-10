28c51532d5e58a7a3cb510dc7b8c52d268efbe9b
Arun <imarunsinghal@gmail.com>
2017-04-10 18:50:31 +0530

    Changes for Image automation

--- new_author_first_quotes_copy.sh
+++ new_author_first_quotes_copy.sh
@@ -1,5 +1,5 @@
 
-[ -z $2 ] && { echo "Usage: sh new_author_first_quotes_copy.sh <Author Id> <Quote Number> <Photo Absolute Path>"; exit 1; }
+[ -z $4 ] && { echo "Usage: sh new_author_first_quotes_copy.sh <Author Id> <Quote Number> <Photo Absolute Path> <Flop 0 or 1>"; exit 1; }
 
 QuotesAuthorId=$1
 QuotesNumber=$2
@@ -48,9 +48,21 @@ sed -i 's/new-author/$QuotesAuthorId/g' ${SampleURL}/sm_comment_new_author_main_
 mv ${SampleURL}/sm_comment_new_author_main_modify.php data/sm_comment/author/$1/sm_comment_$1.php
 
 #Data, Need to Edit
-cp $3/profile_$1.jpg data/images/author/$1/profile/$1.jpg
 
-cp $3/page_cover_$1.jpg data/images/author/$1/page_cover/quotes/$1.jpg
+cp $3/Quotes/$QuotesAuthorId/1/1_*.jpg data/images/author/$1/profile/$1.jpg
 
-cp $3/$1_Quotes.jpg data/images/${URL}/$2/$1-Quotes.jpg
+mogrify  -resize 100x90 data/images/author/$1/profile/$1.jpg
+
+if [ "$4" = "1" ] then 
+mogrify -flop data/images/author/$1/profile/$1.jpg
+fi
+
+convert $3/Quotes/$QuotesAuthorId/1/2_*.png $3/Quotes/$QuotesAuthorId/1/${1}_page_cover.jpg
+jpegoptim --size=65% $3/Quotes/$QuotesAuthorId/1/${1}_page_cover.jpg
+cp $3/Quotes/$QuotesAuthorId/1/${1}_page_cover.jpg data/images/author/$1/page_cover/quotes/$1.jpg
+
+
+convert $3/Quotes/$QuotesAuthorId/1/3_*.png $3/Quotes/$QuotesAuthorId/1/${1}_Quotes.jpg
+jpegoptim --size=65% $3/Quotes/$QuotesAuthorId/1/${1}_Quotes.jpg
+cp $3/Quotes/$QuotesAuthorId/1/${1}_Quotes.jpg data/images/author/$1/page_cover/quotes/$1.jpg
 
