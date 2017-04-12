307483e7c13f0b392b56f493a2010564d5c8fd4d
Arun <imarunsinghal@gmail.com>
2017-04-12 18:25:59 +0530

    Adding Changes for offers

--- data/main_page_content/main_page_content_offers.php
+++ data/main_page_content/main_page_content_offers.php
@@ -4,22 +4,31 @@
     <!-- ################################################################################################ -->
     <div class="content"> 
       <!-- ################################################################################################ -->
-      <h1>Courses</h1>
+      <h1>Offers</h1>
         <div id="gallery">
         <figure>
           <ul class="nospace clear">
-            <li class="one_quarter first"><a href="http://tracking.payoom.com/aff_c?offer_id=193&aff_id=36538" target="_blank"><img src="../data/images/offers/paytm.jpg" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter first"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter first"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
-            <li class="one_quarter"><a href="#"><img src="../images/demo/gallery/01.png" alt=""></a></li>
+            <li class="one_quarter first"><h2 class="heading_h2 nospace btmspace-10 uppercase center">Paytm</h2><a href="http://tracking.payoom.com/aff_c?offer_id=193&aff_id=36538" target="_blank"><img src="../data/images/offers/paytm.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">myntra</h2><a href="https://linksredirect.com/?pub_id=15987CL14359&source=linkkit&url=http%3A//www.myntra.com/" target="_blank"><img src="../data/images/offers/myntra.png" alt=""></a></li>
+
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">jabong</h2><a href="http://tracking.payoom.com/aff_c?offer_id=62&aff_id=36538" target="_blank"><img src="../data/images/offers/jabong.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">ebay</h2><a href="http://tracking.payoom.com/aff_c?offer_id=373&aff_id=36538" target="_blank"><img src="../data/images/offers/ebay.jpg" alt=""></a></li>
+            <li class="one_quarter first"><h2 class="heading_h2 nospace btmspace-10 uppercase center">make my trip</h2><a href="http://tracking.payoom.com/aff_c?offer_id=906&aff_id=36538" target="_blank"><img src="../data/images/offers/makemytrip.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">yatra</h2><a href="http://tracking.payoom.com/aff_c?offer_id=363&aff_id=36538" target="_blank"><img src="../data/images/offers/yatra.jpg" alt=""></a></li>
+
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">bigbasket</h2><a href="http://tracking.payoom.com/aff_c?offer_id=1172&aff_id=36538" target="_blank"><img src="../data/images/offers/bigbasket.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">zoomcar</h2><a href="http://tracking.payoom.com/aff_c?offer_id=640&aff_id=36538" target="_blank"><img src="../data/images/offers/zoomcar.jpg" alt=""></a></li>
+
+            <li class="one_quarter first"><h2 class="heading_h2 nospace btmspace-10 uppercase center">swiggy</h2><a href="http://tracking.payoom.com/aff_c?offer_id=699&aff_id=36538 " target="_blank"><img src="../data/images/offers/swiggy.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">dominos</h2><a href="http://tracking.payoom.com/aff_c?offer_id=217&aff_id=36538" target="_blank"><img src="../data/images/offers/dominos.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">ajio</h2><a href="http://tracking.payoom.com/aff_c?offer_id=1210&aff_id=36538" target="_blank"><img src="../data/images/offers/ajio.jpg" alt=""></a></li>
+
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">shopclues</h2><a href="https://linksredirect.com/?pub_id=15987CL14359&source=linkkit&url=http%3A//www.shopclues.com/" target="_blank"><img src="../data/images/offers/shopclues.jpg" alt=""></a></li>
+            <li class="one_quarter first"><h2 class="heading_h2 nospace btmspace-10 uppercase center">firstcry</h2><a href="http://tracking.payoom.com/aff_c?offer_id=20&aff_id=36538" target="_blank"><img src="../data/images/offers/firstcry.jpg" alt=""></a></li>
+
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">Infibeam</h2><a href="http://tracking.payoom.com/aff_c?offer_id=812&aff_id=36538" target="_blank"><img src="../data/images/offers/Infibeam.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">pepperfry</h2><a href="http://tracking.payoom.com/aff_c?offer_id=86&aff_id=36538" target="_blank"><img src="../data/images/offers/pepperfry.jpg" alt=""></a></li>
+            <li class="one_quarter"><h2 class="heading_h2 nospace btmspace-10 uppercase center">voonik</h2><a href="http://tracking.payoom.com/aff_c?offer_id=1121&aff_id=36538" target="_blank"><img src="../data/images/offers/voonik.png" alt=""></a></li>
           </ul>
         </figure>
       </div> 
@@ -33,7 +42,7 @@
 
 <div id="facebook_share">
     <div class="sm_share">
-        <div class="fb-share-button" data-href="http://www.hellocuriousbrain.com/courses/courses.php" data-layout="box_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.hellocuriousbrain.com%2Fcourses%2Fcourses.php&amp;src=sdkpreparse">Share</a></div>
+        <div class="fb-share-button" data-href="http://www.hellocuriousbrain.com/offers/offers.php" data-layout="box_count" data-size="small" data-mobile-iframe="true"><a class="fb-xfbml-parse-ignore" target="_blank" href="https://www.facebook.com/sharer/sharer.php?u=http%3A%2F%2Fwww.hellocuriousbrain.com%2Foffers%2Foffers.php&amp;src=sdkpreparse">Share</a></div>
     </div>
 </div>
 
---
+++ data/title/title_offers.php
@@ -0,0 +1,1 @@
+<title> Offers - Curious Brain </title>
