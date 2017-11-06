60ded793f3c0b7d371c83c305566c882f82da0f5
Arun <imarunsinghal@gmail.com>
2017-11-06 11:24:08 +0530

    Adding Changes for lesson#1

--- data/page_cover/courses/hindi/hindi-handwriting/in-sequence/lesson-1/page_cover_hindi-handwriting-lesson-1.php
+++ data/page_cover/courses/hindi/hindi-handwriting/in-sequence/lesson-1/page_cover_hindi-handwriting-lesson-1.php
@@ -3,15 +3,15 @@
 function page_cover_fun($currentCourseName,$currentChapterName, $currentType, $currentLessonNumber, $currpageId) {
    ?>
     
-       <div class="wrapper cover_bgded cover_overlay" style="background-image:url('../../../../../data/images/courses/<?php echo$currentCourseName ?>/<?php echo$currentChapterName ?>/<?php echo$currentType ?>/<?php echo$currentLessonNumber ?>/page_cover/<?php echo $currpageId  ?>.png');">
+       <div class="wrapper cover_bgded cover_overlay" style="background-image:url('../../../../../data/images/courses/<?php echo$currentCourseName ?>/<?php echo$currentChapterName ?>/<?php echo$currentType ?>/<?php echo$currentLessonNumber ?>/page_cover/<?php echo $currpageId  ?>.jpg');">
   <div id="breadcrumb" class="hoc clear"> 
     <!-- ################################################################################################ -->
     <ul>
       <li><a href="../../../../../index.php">Home</a></li>
       <li><a href="../../../../../courses/courses.php">Courses</a></li>
-      <li><a href="../../../../../courses/english/english.php">English</a></li>
-      <li><a href="../../../../../courses/english/cursive-handwriting/cursive_handwriting.php">cursive handwriting</a></li>
-      <li><a href="../../../../../courses/english/cursive-handwriting/alphabet-sequence/lesson-1/cursive-handwriting-capital-letters-A-I.php">cursive handwriting lesson#1</a></li>
+      <li><a href="../../../../../courses/<?php echo$currentCourseName ?>/<?php echo$currentCourseName?>.php">Hindi</a></li>
+      <li><a href="../../../../../courses/<?php echo$currentCourseName ?>/<?php echo$currentChapterName ?>/<?php echo$currentChapterName ?>.php">Hindi handwriting</a></li>
+      <li><a href="../../../../../courses/<?php echo$currentCourseName ?>/<?php echo$currentChapterName ?>/<?php echo$currentType ?>/<?php echo$currentLessonNumber ?>/<?php echo$currpageId ?>.php">Hindi handwriting lesson#1</a></li>
     </ul>
     <!-- ################################################################################################ -->
   </div>
