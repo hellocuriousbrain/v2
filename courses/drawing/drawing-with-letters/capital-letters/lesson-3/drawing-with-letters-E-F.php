e96fe481557650ab28193987a29a71ddf6a2ec93
Arun <imarunsinghal@gmail.com>
2017-11-10 17:55:15 +0530

    Changes for Drawing with Lesson3,4 page

---
+++ courses/drawing/drawing-with-letters/capital-letters/lesson-3/drawing-with-letters-E-F.php
@@ -0,0 +1,14 @@
+<?php
+    // Url structure
+    //courses/english/cursive-handwriting/alphabet-sequence/lesson-1/cursive-handwriting-capital-letters-A-I.php
+    $path = $_SERVER['REQUEST_URI']; // this gives you /folder1/folder2/THIS_ONE/file.php
+    $folders = explode("/", $path); // splits folders in array
+    $currentCourseName = $folders[2];
+    $currentChapterName = $folders[3];
+    $currentType = $folders[4];
+    $currentLessonNumber = $folders[5];
+	$currpage = substr($_SERVER["SCRIPT_NAME"],strrpos($_SERVER["SCRIPT_NAME"],"/")+1);
+	$currpageId = substr($currpage, 0 , -4);
+	include '../../../../../data/common/misc/l5_sub_page.php';
+	new_page($currentCourseName,$currentChapterName, $currentType, $currentLessonNumber, $currpageId); 
+?>
