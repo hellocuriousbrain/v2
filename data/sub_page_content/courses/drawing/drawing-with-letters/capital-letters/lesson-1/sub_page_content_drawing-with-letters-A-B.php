c5bc316eb3861e3cc51de27ee6d07b6568107e22
Arun <imarunsinghal@gmail.com>
2017-11-10 13:25:19 +0530

    Changes for Drawing with A and B page

--- data/sub_page_content/courses/drawing/drawing-with-letters/capital-letters/lesson-1/sub_page_content_drawing-with-letters-A-B.php
+++ data/sub_page_content/courses/drawing/drawing-with-letters/capital-letters/lesson-1/sub_page_content_drawing-with-letters-A-B.php
@@ -35,24 +35,120 @@ How to Draw Kite using letter A ?
       <p class="bold">
 How to Draw Monkey using letter B ?
 </p>
-      <p>
-In this lesson we are going to make a kite and a monkey using capital letter A and B.
+      <p class="bold"   >
+In this lesson we are going to make a kite and a monkey using capital letter A and B. For this you need a drawing sheet and some colors. 
 </p>
-      <p>
-So friends, what are you waiting for ? Get ready with you tools and let's draw.
-</p>
-
-<br>
 <h2 class="heading_h2">
- Steps and Methods for Drawing using A and B are explained in below <a href="https://www.youtube.com/watch?v=6uJg9LrsEsw" target="_blank" title="Drawing Lessons">youtube Video</a> :-
+Let's make kite using A
 </h2>
+<h2 class="heading_h2">
+Steps :-
+</h2>
+      <p class="bold">
+1) First of all make A :-
+</p>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-A-step-1.jpg" alt="">
+
+      <p class="bold">
+2) Make One more A in opposite down to it and then Draw a line in middle between two corners.
+</p>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-A-step-2.jpg" alt="">
+<br>
+<div class="borderedbox_videotutorial center">
+<h2 class="heading_h2">Video Tutorial is also available <br>
+</h2>
+<p class="font_size_20" >
+ Steps and Methods for Drawing using A and B are explained in below <a href="https://www.youtube.com/watch?v=6uJg9LrsEsw" target="_blank" title="Drawing Lessons">youtube Video</a> :- </p>
 <div class="center">
 <iframe width="320" height="180" src="https://www.youtube.com/embed/6uJg9LrsEsw" frameborder="0" allowfullscreen></iframe>
 </div>
+</div>
 <br>
+
+      <p class="bold">
+3) Now let’s do some creativity. Now we are going to make eyes, nose and mouth and kite string.
+</p>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-A-step-3.jpg" alt="">
+
+      <p class="bold">
+4) Now we are going to fill color. You can fill color of your choice also. I am filling red color in mouth,skyblue color in the Top, yellow, orange, pink color in other parts of kite . 
+
+</p>
+<div class="borderedbox_quotes center">
+<h3 class="heading_h3">
+Curious Brain Tips :-
+</h3>
+<p class="font_size_20" >
+Tips while filling color, Make first inner line then start filling color inside. With this your color won’t get outside and you will be fill color easily.
+</p>
+</div>
+<br>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-A-step-4.jpg" alt="">
+<br>
+
+<p class="font_size_20" >
+Friends, I am using wax crayons for coloring, You can use pencil or sketch color whatever is available with you for coloring.
+</p>
+
+<br>
+<br>
+<h2 class="heading_h2">
+Let's make Monkey using B
+</h2>
+<h2 class="heading_h2">
+Steps :-
+</h2>
+      <p class="bold">
+1) First of all make B :-
+</p>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-B-step-1.jpg" alt="">
+
+      <p class="bold">
+2) Now make money Eyes, and eyebrows. Then make money nose.Then mouth.
+</p>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-B-step-2.jpg" alt="">
+<br>
+<div class="borderedbox_videotutorial center">
+<h2 class="heading_h2">Video Tutorial is also available <br>
+</h2>
+<p class="font_size_20" >
+ Steps and Methods for Drawing using A and B are explained in below <a href="https://www.youtube.com/watch?v=6uJg9LrsEsw" target="_blank" title="Drawing Lessons">youtube Video</a> :- </p>
+<div class="center">
+<iframe width="320" height="180" src="https://www.youtube.com/embed/6uJg9LrsEsw" frameborder="0" allowfullscreen></iframe>
+</div>
+</div>
+<br>
+
+      <p class="bold">
+3) Then make Monkey Cap. Use A for make cap.Then make monkey Hairs.
+</p>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-B-step-3.jpg" alt="">
+
+      <p class="bold">
+4) Now make a little bold outline, and fill color in it, Fill red color in mouth and nose, fill sky blue in cap, use black color to make hair thicker.
+Now filling brown color in the body parts of Monkey. Use same tip to fill color, which was given while making A.
+</p>
+<div class="borderedbox_quotes center">
+<h3 class="heading_h3">
+Curious Brain Tips :-
+</h3>
+<p class="font_size_20" >
+Tips while filling color, Make first inner line then start filling color inside. With this your color won’t get outside and you will be fill color easily.
+</p>
+</div>
+<br>
+<img class="imgm borderedbox inspace-5" src="../../../../../data/images/courses/drawing/drawing-with-letters/capital-letters/lesson-1/drawing-using-B-step-4.jpg" alt="">
+<br>
+
+<p>
+So friends, here is your monkey made with B.
+</p>
+
+<p>
+This is the complete lesson to  draw using A and B.
 <br>
 <p>
-We hope it will help to improve your drawing. We will be explaining from C and D in next video lesson.
+In the <a href="../lesson-2/drawing-with-letters-C-D.php" title="how to make fish and parrot using C and D">next lesson</a> we will learn that how to make fish and parrot using C and D in next lesson. We hope it will help to improve your drawing. 
 </p>
 <p>
 Please leave comments in below section.
