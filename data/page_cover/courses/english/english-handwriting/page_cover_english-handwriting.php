d3988b58a538bc08d2c0c330dc9e942cb8c71310
Arun <imarunsinghal@gmail.com>
2018-03-28 19:44:08 +0530

    Changes for minor photo changes

--- data/page_cover/courses/english/english-handwriting/page_cover_english-handwriting.php
+++ data/page_cover/courses/english/english-handwriting/page_cover_english-handwriting.php
@@ -3,14 +3,14 @@
 function page_cover_fun($currentCourseName,$currentChapterName,$currpageId) {
    ?>
     
-       <div class="wrapper cover_bgded cover_overlay" style="background-image:url('../../../data/images/courses/<?php echo$currentCourseName ?>/<?php echo$currentChapterName ?>/<?php echo $currpageId  ?>.png');">
+       <div class="wrapper cover_bgded cover_overlay" style="background-image:url('../../../data/images/courses/<?php echo$currentCourseName ?>/<?php echo$currentChapterName ?>/<?php echo $currpageId  ?>.jpg');">
   <div id="breadcrumb" class="hoc clear"> 
     <!-- ################################################################################################ -->
     <ul>
       <li><a href="../../../index.php">Home</a></li>
       <li><a href="../../../courses/courses.php">Courses</a></li>
       <li><a href="../../../courses/english/english.php">English</a></li>
-      <li><a href="../../../courses/english/english-handwriting/english_handwriting.php">English handwriting</a></li>
+      <li><a href="../../../courses/english/english-handwriting/english-handwriting.php">English handwriting</a></li>
     </ul>
     <!-- ################################################################################################ -->
   </div>
